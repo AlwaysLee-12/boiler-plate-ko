87d6435f281f9058a5a9630edda0be568fc9bd04
AlwaysLee-Creater <54054460+AlwaysLee-Creater@users.noreply.github.com>
2021-04-26 14:17:41 +0900

    axios 예제 적용 실습

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 function LandingPage() {
+    useEffect(()=>{
+        //서버에 GET 요청을 보내고, 그에 따른 응답을 콘솔 창에 전시
+        axios.get('/api/hello')
+        .then(response=>console.log(response.date));
+    },[]);
+
     return (
         <div>
             LandingPage
--- server/index.js
+++ server/index.js
@@ -20,6 +20,10 @@ mongoose.connect(config.mongoURI,{
 
 app.get('/',(req,res)=>res.send('Hello World!!!'));
 
+app.get('/api/hello',(req,res)=>{
+  res.send("안녕하세요~");
+})
+
 app.post('/api/users/register',(req,res)=>{
   //회원 가입 시 필요한 정보들을 client에서 가져와 데이터 베이스에 저장
   const user=new User(req.body);
