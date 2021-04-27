d359a94133e23c8eae6d758904f15f6a9aa600b4
AlwaysLee-Creater <54054460+AlwaysLee-Creater@users.noreply.github.com>
2021-04-27 21:43:02 +0900

    프록시 설정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
 function LandingPage() {
-    useEffect(()=>{
-        //서버에 GET 요청을 보내고, 그에 따른 응답을 콘솔 창에 전시
+    //서버에 GET 요청을 보내고, 그에 따른 응답을 콘솔 창에 전시
+    useEffect(() => {
         axios.get('/api/hello')
-        .then(response=>console.log(response.date));
-    },[]);
+        .then(response=>{console.log(response)});
+    }, [])
 
     return (
         <div>
-            LandingPage
+            LandingPage 랜딩 페이지
         </div>
     )
 }
