39c8b4a9e1ca038765832371a07c4d74bfcec52f
AlwaysLee-Creater <54054460+AlwaysLee-Creater@users.noreply.github.com>
2021-05-01 10:25:33 +0900

    로그아웃 페이지 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,24 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
     //서버에 GET 요청을 보내고, 그에 따른 응답을 콘솔 창에 전시
     useEffect(() => {
         axios.get('/api/hello')
         .then(response=>{console.log(response)});
     }, [])
+
+    const onclickHandler=()=>{
+        axios.get('/api/users/logout')
+        .then(response=>{
+            if(response.data.success){
+                console.log(response.data);
+                props.history.push('/login')
+            }else{
+                alert("로그아웃 실패")
+            }
+        })
+    }
 
     return (
         <div style={{
@@ -14,6 +26,10 @@ function LandingPage() {
             , width:'100%', height:'100vh'
         }}>
             <h2>시작 페이지</h2>
+
+            <button onClick={onclickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
